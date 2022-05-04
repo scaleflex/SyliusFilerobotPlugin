0516de7e2caa1ef7a5c04459434ac638c825b7aa
Tung Dang <tungdt163@gmail.com>
2022-05-04 09:56:08 +0700

    init

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace Scaleflex\SyliusFilerobotPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
--- src/ScaleflexSyliusFilerobotPlugin.php
+++ src/ScaleflexSyliusFilerobotPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Scaleflex\SyliusFilerobotPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class ScaleflexSyliusFilerobotPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
